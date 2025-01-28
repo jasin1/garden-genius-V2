0363986790b9ad3a835ed2fbf0318e98f7717471
jasin1 <jasin.tairaidrissi@gmail.com>
2025-01-28 12:31:17 +0100

    fix responsive issues with searchBar

--- src/components/SeachBar/SearchBar.jsx
+++ src/components/SeachBar/SearchBar.jsx
@@ -2,6 +2,7 @@ import './SearchBar.css';
 import PropTypes from "prop-types";
 import {useState} from "react";
 import SearchIcon from '../../assets/icon-search.svg';
+import SearchIconWhite from '../../assets/icon-search-white.svg';
 import Button from '../../components/Button/Button.jsx';
 
 function SearchBar({onSearch, placeholder, initialValue}){
@@ -36,7 +37,10 @@ function SearchBar({onSearch, placeholder, initialValue}){
                 onChange={handleInputChange}
                 onKeyDown={handleKeyPress}
             />
-            <Button type="button" variant="search" onClick={handleSearch}> Search </Button>
+            <Button type="button" className="search-button" variant="search" onClick={handleSearch}> <span className="search-text">Search</span> 
+            <img src={SearchIconWhite} alt="Search Icon" className="search-icon" />
+            </Button>
+
         </div>
 
     )
